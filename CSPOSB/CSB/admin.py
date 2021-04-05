22e5bae00590570286904d7400de0913d80ad2dc
Will Kunz <wleelanau@gmail.com>
2021-04-05 11:20:40 -0400

    Added all models and minimal data

--- CSPOSB/CSB/admin.py
+++ CSPOSB/CSB/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Course, Requirement, Degree, Fulfills, Requires
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Requires)
+admin.site.register(Requirement)
+admin.site.register(Degree)
+admin.site.register(Fulfills)
--- CSPOSB/CSB/models.py
+++ CSPOSB/CSB/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    cid = models.IntegerField(unique=True, primary_key=True)
+    name = models.TextField(unique=True)
+    number = models.IntegerField()
+    semester = models.TextField()
+    department = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Requirement(models.Model):
+    rid = models.IntegerField(unique=True, primary_key=True)
+    name = models.TextField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Degree(models.Model):
+    did = models.IntegerField(unique=True, primary_key=True)
+    name = models.TextField(unique=True)
+    total_credits = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Fulfills(models.Model):
+    cid = models.ForeignKey(Course, on_delete=models.CASCADE)
+    rid = models.ForeignKey(Requirement, on_delete=models.CASCADE)
+    concentration = models.TextField(default="n/a")
+
+    def __str__(self):
+        return f'Course: {self.cid} | Requirement: {self.rid}'
+
+    class Meta:
+        verbose_name_plural = "Fulfills"
+
+
+class Requires(models.Model):
+    did = models.ForeignKey(Degree, on_delete=models.CASCADE)
+    rid = models.ForeignKey(Requirement, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'Degree: {self.did} | Requirement: {self.rid}'
+
+    class Meta:
+        verbose_name_plural = "Requires"
