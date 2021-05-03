254381e9fc93c25f527440c57e0ce0c7aad83d56
Will Kunz <wleelanau@gmail.com>
2021-05-03 16:28:34 -0400

    added tests to views and fixed visual error

--- CSPOSB/CSPOSB/tests/test_views.py
+++ CSPOSB/CSPOSB/tests/test_views.py
@@ -1,8 +1,35 @@
 import unittest
+from django.test import TestCase, Client
 from CSB.views import checkRequirements
+from django.urls import reverse
+
 
 class TestRC(unittest.TestCase):
     def test_checkRequirements(self):
         classes = []
         self.assertEqual(checkRequirements(classes),
-                         (False, ["Total CS Credits", "Total CS Courses", "Core Courses", "Breadth Courses", "Depth Courses"]))
+                         (False,
+                          ["Total CS Credits", "Total CS Courses", "Core Courses", "Breadth Courses", "Depth Courses"]))
+
+
+class TestViews(TestCase):
+    def test_index_GET(self):
+        client = Client()
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_posb(self):
+        client = Client()
+        response = client.get(reverse('PoSB'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'ProgramBuilder.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_rChecker(self):
+        client = Client()
+        response = client.get(reverse('RChecker'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'RChecker.html')
+        self.assertTemplateUsed(response, 'base.html')
