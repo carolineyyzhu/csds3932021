af102413cb70f580b25a551dde90a3e3b92904f7
warmathj <75702254+warmathj@users.noreply.github.com>
2021-05-02 23:30:08 -0400

    moved the requirement checker code from posb to rchecker in view.py; added schedule output for posb

--- CSPOSB/CSB/views.py
+++ CSPOSB/CSB/views.py
@@ -22,6 +22,141 @@ def posb(request):
     for courses in breadthClasses:
         cid.append(courses.cid)
         name.append(courses.name)
+        temp = courses.department + str(courses.number)
+        numberDept.append(temp)
+    breadthToHTML1 = [cid, name, numberDept]
+    cid = []
+    name = []
+    numberDept = []
+    breadthToHTML = zip(breadthToHTML1[0], breadthToHTML1[1], breadthToHTML1[2])
+
+    # Depth
+    depthClasses = Course.objects.all().select_related().filter(fulfills__rid=1)
+    for courses in depthClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
+        temp = courses.department + " " + str(courses.number)
+        numberDept.append(temp)
+    depthToHTML1 = [cid, name, numberDept]
+    cid = []
+    name = []
+    numberDept = []
+    depthToHTML = zip(depthToHTML1[0], depthToHTML1[1], depthToHTML1[2])
+
+    # Core
+    coreClasses = Course.objects.all().select_related().filter(fulfills__rid=9)
+    for courses in coreClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
+        temp = courses.department + str(courses.number)
+        numberDept.append(temp)
+    coreToHTML1 = [cid, name, numberDept]
+    cid = []
+    name = []
+    numberDept = []
+    coreToHTML = zip(coreToHTML1[0], coreToHTML1[1], coreToHTML1[2])
+
+    # General Breadth
+    generalBreadthClasses = Course.objects.all().select_related().filter(fulfills__rid=8)
+    for courses in generalBreadthClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
+        temp = courses.department + str(courses.number)
+        numberDept.append(temp)
+    generalBreadthToHTML1 = [cid, name, numberDept]
+    cid = []
+    name = []
+    numberDept = []
+    generalBreadthToHTML = zip(generalBreadthToHTML1[0], generalBreadthToHTML1[1], generalBreadthToHTML1[2])
+
+    # Technical Elective
+    techElectiveClasses = Course.objects.all().select_related().filter(fulfills__rid=4)
+    for courses in techElectiveClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
+        temp = courses.department + str(courses.number)
+        numberDept.append(temp)
+    techElectiveToHTML1 = [cid, name, numberDept]
+    cid = []
+    name = []
+    numberDept = []
+    techElectiveToHTML = zip(techElectiveToHTML1[0], techElectiveToHTML1[1], techElectiveToHTML1[2])
+
+    # Sages
+    sagesClasses = Course.objects.all().select_related().filter(fulfills__rid=3)
+    for courses in sagesClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
+        temp = courses.department + str(courses.number)
+        numberDept.append(temp)
+    sagesToHTML1 = [cid, name, numberDept]
+    sagesToHTML = zip(sagesToHTML1[0], sagesToHTML1[1], sagesToHTML1[2])
+    # Schedule has been submitted
+    f1, s1, f2, s2, f3, s3, f4, s4 = ['Fall Year 1'], ['Spring Year 1'], ['Fall Year 2'], ['Spring Year 2'], ['Fall Year 3'], ['Spring Year 3'], ['Fall Year 4'], ['Spring Year 4']
+    if request.method == "POST":
+        form = request.POST.get("builder")
+        data = request.POST.copy()
+        semester = data.getlist("Semester")
+        year = data.getlist("Year")
+        classes = data.getlist('Classes')
+
+        toDelete = []
+        removeEmpty=[]
+        for i in range(0,len(classes)):
+            if classes[i] != "empty":
+                removeEmpty.append(i-1)
+        removeEmpty.reverse()
+        for index in removeEmpty:
+            classes.pop(index)
+        for i in range(0, len(classes)):
+            if classes[i] == "empty":
+                toDelete.append(i)
+        toDelete.reverse()
+        for index in range(0, len(toDelete)):
+            semester.pop(toDelete[index])
+            year.pop(toDelete[index])
+            classes.pop(toDelete[index])
+
+        for i in range(0, len(classes)):
+            if semester[i] == "spring":
+                if year[i] == '1':
+                    s1.append(classes[i])
+                elif year[i] == '2':
+                    s2.append(classes[i])
+                elif year[i] == '3':
+                    s3.append(classes[i])
+                else:
+                    s4.append(classes[i])
+            else:
+                if year[i] == '1':
+                    f1.append(classes[i])
+                elif year[i] == '2':
+                    f2.append(classes[i])
+                elif year[i] == '3':
+                    f3.append(classes[i])
+                else:
+                    f4.append(classes[i])
+
+    sems = [f1, s1, f2, s2, f3, s3, f4, s4]
+    context = {"generalBreadthClasses": generalBreadthToHTML, "coreClasses": coreToHTML,
+               "breadthClasses": breadthToHTML, "depthClasses": depthToHTML, "sagesClasses": sagesToHTML,
+               "techElectiveClasses": techElectiveToHTML, "s1": s1, "s2": s2, "s3": s3, "s4": s4, "f1": f1, "f2": f2,
+               "f3": f3, "f4": f4, "sems": sems}
+
+    return render(request, 'ProgramBuilder.html', context)
+
+
+def rchecker(request):
+    cid = []
+    name = []
+    numberDept = []
+
+    # Gets all classes by requirement for display on schedule builder form
+    # Breadth
+    breadthClasses = Course.objects.all().select_related().filter(fulfills__rid=2)
+    for courses in breadthClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
         temp = courses.department + " " + str(courses.number)
         numberDept.append(temp)
     breadthToHTML1 = [cid, name, numberDept]
@@ -93,7 +228,7 @@ def posb(request):
     sagesToHTML = zip(sagesToHTML1[0], sagesToHTML1[1], sagesToHTML1[2])
 
     # Schedule has been submitted
-    f1, s1, f2, s2, f3, s3, f4, s4 = ([],) * 8
+    f1, s1, f2, s2, f3, s3, f4, s4 = [],[],[],[],[],[],[],[]
     if request.method == "POST":
         form = request.POST.get("builder")
         data = request.POST.copy()
@@ -122,7 +257,7 @@ def posb(request):
             if semester == "spring":
                 if year == 1:
                     s1.append(classes[i])
-                if year == 4:
+                if year == 2:
                     s2.append(classes[i])
                 if year == 3:
                     s3.append(classes[i])
@@ -131,7 +266,7 @@ def posb(request):
             else:
                 if year == 1:
                     f1.append(classes[i])
-                if year == 4:
+                if year == 2:
                     f2.append(classes[i])
                 if year == 3:
                     f3.append(classes[i])
@@ -146,12 +281,7 @@ def posb(request):
         context = {"generalBreadthClasses": generalBreadthToHTML, "coreClasses": coreToHTML,
                    "breadthClasses": breadthToHTML, "depthClasses": depthToHTML, "sagesClasses": sagesToHTML,
                    "techElectiveClasses": techElectiveToHTML}
-        return render(request, 'ProgramBuilder.html', context)
-
-
-def rchecker(request):
-    context = {"fulfillsRequirements":False, "classesNeeded":None}
-    return render(request, 'RequirementChecker.html', context)
+        return render(request, 'RequirementChecker.html', context)
 
 
 def checkRequirements(classes):
