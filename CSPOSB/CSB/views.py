aab32d3a8b7b3a3571a5b1176021e482f0013fc4
Will Kunz <wleelanau@gmail.com>
2021-05-04 11:17:42 -0400

    Added engineering classes requirement to POSB/ Rchecker

--- CSPOSB/CSB/views.py
+++ CSPOSB/CSB/views.py
@@ -82,6 +82,19 @@ def posb(request):
     numberDept = []
     techElectiveToHTML = zip(techElectiveToHTML1[0], techElectiveToHTML1[1], techElectiveToHTML1[2])
 
+    # Engineering
+    engineeringClasses = Course.objects.all().select_related().filter(fulfills__rid=10)
+    for courses in engineeringClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
+        temp = courses.department + " " + str(courses.number)
+        numberDept.append(temp)
+    engineeringToHTML1 = [cid, name, numberDept]
+    cid = []
+    name = []
+    numberDept = []
+    engineeringToHTML = zip(engineeringToHTML1[0], engineeringToHTML1[1], engineeringToHTML1[2])
+
     # Sages
     sagesClasses = Course.objects.all().select_related().filter(fulfills__rid=3)
     for courses in sagesClasses:
@@ -92,7 +105,8 @@ def posb(request):
     sagesToHTML1 = [cid, name, numberDept]
     sagesToHTML = zip(sagesToHTML1[0], sagesToHTML1[1], sagesToHTML1[2])
     # Schedule has been submitted
-    f1c, s1c, f2c, s2c, f3c, s3c, f4c, s4c = ['Fall Year 1'], ['Spring Year 1'], ['Fall Year 2'], ['Spring Year 2'], ['Fall Year 3'], ['Spring Year 3'], ['Fall Year 4'], ['Spring Year 4']
+    f1c, s1c, f2c, s2c, f3c, s3c, f4c, s4c = ['Fall Year 1'], ['Spring Year 1'], ['Fall Year 2'], ['Spring Year 2'], [
+        'Fall Year 3'], ['Spring Year 3'], ['Fall Year 4'], ['Spring Year 4']
     f1n, s1n, f2n, s2n, f3n, s3n, f4n, s4n = [''], [''], [''], [''], [''], [''], [''], ['']
     if request.method == "POST":
         form = request.POST.get("builder")
@@ -103,10 +117,10 @@ def posb(request):
         names = data.getlist('Name')
 
         toDelete = []
-        removeEmpty=[]
-        for i in range(0,len(classes)):
+        removeEmpty = []
+        for i in range(0, len(classes)):
             if classes[i] != "empty":
-                removeEmpty.append(i-1)
+                removeEmpty.append(i - 1)
         removeEmpty.reverse()
         for index in removeEmpty:
             classes.pop(index)
@@ -155,14 +169,15 @@ def posb(request):
 
     pairs = []
 
-    for i in range(0,len(classes)):
+    for i in range(0, len(classes)):
         pair = [classes[i], names[i]]
         pairToHtml = zip(pair[0], pair[1])
         pairs.append(pairToHtml)
 
     context = {"generalBreadthClasses": generalBreadthToHTML, "coreClasses": coreToHTML,
                "breadthClasses": breadthToHTML, "depthClasses": depthToHTML, "sagesClasses": sagesToHTML,
-               "techElectiveClasses": techElectiveToHTML, "responsePairs" : pairs}
+               "techElectiveClasses": techElectiveToHTML, "engineeringClasses": engineeringToHTML,
+               "responsePairs": pairs}
 
     return render(request, 'ProgramBuilder.html', context)
 
@@ -238,6 +253,19 @@ def rchecker(request):
     numberDept = []
     techElectiveToHTML = zip(techElectiveToHTML1[0], techElectiveToHTML1[1], techElectiveToHTML1[2])
 
+    # Engineering
+    engineeringClasses = Course.objects.all().select_related().filter(fulfills__rid=10)
+    for courses in engineeringClasses:
+        cid.append(courses.cid)
+        name.append(courses.name)
+        temp = courses.department + " " + str(courses.number)
+        numberDept.append(temp)
+    engineeringToHTML1 = [cid, name, numberDept]
+    cid = []
+    name = []
+    numberDept = []
+    engineeringToHTML = zip(engineeringToHTML1[0], engineeringToHTML1[1], engineeringToHTML1[2])
+
     # Sages
     sagesClasses = Course.objects.all().select_related().filter(fulfills__rid=3)
     for courses in sagesClasses:
@@ -255,10 +283,10 @@ def rchecker(request):
         classes = data.getlist('Classes')
 
         toDelete = []
-        removeEmpty=[]
-        for i in range(0,len(classes)):
+        removeEmpty = []
+        for i in range(0, len(classes)):
             if classes[i] != "empty":
-                removeEmpty.append(i-1)
+                removeEmpty.append(i - 1)
         removeEmpty.reverse()
         for index in removeEmpty:
             classes.pop(index)
@@ -267,7 +295,6 @@ def rchecker(request):
                 toDelete.append(i)
         toDelete.reverse()
         for index in range(0, len(toDelete)):
-
             classes.pop(toDelete[index])
 
         areReqFulfilled, missingReq = checkRequirements(classes)
@@ -277,59 +304,60 @@ def rchecker(request):
     else:
         context = {"generalBreadthClasses": generalBreadthToHTML, "coreClasses": coreToHTML,
                    "breadthClasses": breadthToHTML, "depthClasses": depthToHTML, "sagesClasses": sagesToHTML,
-                   "techElectiveClasses": techElectiveToHTML}
+                   "techElectiveClasses": techElectiveToHTML, "engineeringClasses": engineeringToHTML}
         return render(request, 'RChecker.html', context)
 
 
 def checkRequirements(classes):
-    while('empty' in classes):
+    while ('empty' in classes):
         classes.remove('empty')
 
-    #TODO: Get the actual value of the depth area
-    #TODO: Add group 1 and group 2 to database
+    # TODO: Get the actual value of the depth area
+    # TODO: Add group 1 and group 2 to database
     depth_area = "Software Engineering"
     are_reqs_fulfilled = True
     missing_reqs = []
 
-    cs_requirement_nums = {"total_cs_credits":0, "total_cs_courses":0, "core_courses":0, "breadth_courses":0, "depth_courses":0, "tech_group_1": 0, "tech_group_2":0}
-    can_be_counted = {"depth":False, "breadth":False, "tech-elective":False}
+    cs_requirement_nums = {"total_cs_credits": 0, "total_cs_courses": 0, "core_courses": 0, "breadth_courses": 0,
+                           "depth_courses": 0, "tech_group_1": 0, "tech_group_2": 0}
+    can_be_counted = {"depth": False, "breadth": False, "tech-elective": False}
 
     for course in classes:
         dept_id = course[0:4]
         course_id = course[5:8]
 
-        if Course.objects.filter(department=dept_id,number=course_id).exists():
+        if Course.objects.filter(department=dept_id, number=course_id).exists():
             cs_requirement_nums["tech_group_1"] += 1
 
-        if dept_id == "CSDS" and Course.objects.filter(department=dept_id,number=course_id).exists():
-            cs_requirement_nums["total_cs_credits"] += Course.objects.get(department=dept_id,number=course_id).credits
+        if dept_id == "CSDS" and Course.objects.filter(department=dept_id, number=course_id).exists():
+            cs_requirement_nums["total_cs_credits"] += Course.objects.get(department=dept_id, number=course_id).credits
             cs_requirement_nums["total_cs_courses"] += 1
 
         course_name = Course.objects.get(department=dept_id, number=course_id).cid
-        #TODO: make sure that this is the same as the depth from above
+        # TODO: make sure that this is the same as the depth from above
         can_be_counted["depth"] = Fulfills.objects.get(cid=course_name).rid == "Depth"
         can_be_counted["breadth"] = Fulfills.objects.get(cid=course_name).rid == "Breadth"
-
 
         if can_be_counted["depth"]:
             cs_requirement_nums["depth_courses"] += 1
         if can_be_counted["breadth"]:
             cs_requirement_nums["breadth_courses"] += 1
-        #TODO: Fulfills needs classes that can be counted toward the tech electives
-        #elif not can_be_counted["depth"]:
+        # TODO: Fulfills needs classes that can be counted toward the tech electives
+        # elif not can_be_counted["depth"]:
 
-    is_fulfilled = {"is_cs_credits_fulfilled":cs_requirement_nums["total_cs_credits"] > 63,
-                    "is_cs_courses_fulfilled":cs_requirement_nums["total_cs_courses"] > 20,
-                    "is_core_fulfilled":cs_requirement_nums["core_courses"] > 6,
-                    "is_breadth_fulfilled":cs_requirement_nums["breadth_courses"] > 5,
-                    "is_depth_fulfilled":cs_requirement_nums["depth_courses"] > 4,
-                    "is_tech_fulfilled":cs_requirement_nums["tech_group_1"] + cs_requirement_nums["tech_group_2"] <= 6 and cs_requirement_nums["tech_group_2"] <= 2}
+    is_fulfilled = {"is_cs_credits_fulfilled": cs_requirement_nums["total_cs_credits"] > 63,
+                    "is_cs_courses_fulfilled": cs_requirement_nums["total_cs_courses"] > 20,
+                    "is_core_fulfilled": cs_requirement_nums["core_courses"] > 6,
+                    "is_breadth_fulfilled": cs_requirement_nums["breadth_courses"] > 5,
+                    "is_depth_fulfilled": cs_requirement_nums["depth_courses"] > 4,
+                    "is_tech_fulfilled": cs_requirement_nums["tech_group_1"] + cs_requirement_nums[
+                        "tech_group_2"] <= 6 and cs_requirement_nums["tech_group_2"] <= 2}
 
     if not is_fulfilled["is_cs_credits_fulfilled"]:
         missing_reqs.append("Total CS Credits")
     if not is_fulfilled["is_cs_courses_fulfilled"]:
         missing_reqs.append("Total CS Courses")
-        #TODO: display all technical electives to fulfill this requirement
+        # TODO: display all technical electives to fulfill this requirement
     if not is_fulfilled["is_core_fulfilled"]:
         missing_reqs.append("Core Courses")
     if not is_fulfilled["is_breadth_fulfilled"]:
